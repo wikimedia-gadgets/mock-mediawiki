54e93091ac45dae7c15984175150ae5a735cab33
Siddharth VP <siddharthvp@gmail.com>
2021-02-24 22:07:46 +0530

    Add wgLegalTitleChars to fix mw.Title, add tests

---
+++ tests/lib.test.js
@@ -0,0 +1,16 @@
+
+describe('test', function () {
+
+	test('mw.Title works with correct wgLegalTitleChars mocking', () => {
+		expect(new mw.Title('Template:Foo').getMainText()).toBe('Foo');
+		expect(mw.Title.newFromText('File:Michał Cieślak Sejm 2016.JPG')).not.toBeNull();
+		expect(mw.Title.newFromText('Talk:Template:Foo')).toBeNull();
+	});
+
+	test('Other stuff', () => {
+		expect($.extend).toBeInstanceOf(Function);
+		expect(mw.config).toBeInstanceOf(mw.Map);
+		expect(mw.util.escapeRegExp("a?:$")).toBe("a\\?:\\$");
+	});
+
+});
